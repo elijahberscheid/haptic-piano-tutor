b915d0d9b7e00fe41bb6f53712f202e3baf4cd19
Carrie Li <licarrie02@gmail.com>
2022-02-11 18:37:43 -0500

    Glue music manager into main with C major scale

--- stm32f091/src/modules/Song.c
+++ stm32f091/src/modules/Song.c
@@ -1,39 +1,81 @@
 #include "Song.h"
 
-// static const Note_t channel1[] = {
-//     { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
-//     { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
-//     { .key = Key_C4, .length = NoteLength_Quarter, .finger = Finger_Right1 },
-//     { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
-// 
-//     { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
-//     { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
-//     { .key = Key_E4, .length = NoteLength_Half, .finger = Finger_Right3 },
-// 
-//     { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
-//     { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
-//     { .key = Key_D4, .length = NoteLength_Half, .finger = Finger_Right2 },
-// 
-//     { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
-//     { .key = Key_G4, .length = NoteLength_Quarter, .finger = Finger_Right5 },
-//     { .key = Key_G4, .length = NoteLength_Half, .finger = Finger_Right5 },
-// 
-//     { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
-//     { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
-//     { .key = Key_C4, .length = NoteLength_Quarter, .finger = Finger_Right1 },
-//     { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
-// 
-//     { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
-//     { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
-//     { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
-//     { .key = Key_C4, .length = NoteLength_Quarter, .finger = Finger_Right1 },
-// 
-//     { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
-//     { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
-//     { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
-//     { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
-// 
-//     { .key = Key_C4, .length = NoteLength_Whole, .finger = Finger_Right1 },
-//
-//     { .key = Key_Invalid, .length = 0, .finger = 0 }
-// };
+const Note_t maryHadALittleLamb1[] = {
+    { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+    { .key = Key_C4, .length = NoteLength_Quarter, .finger = Finger_Right1 },
+    { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+
+    { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_E4, .length = NoteLength_Half, .finger = Finger_Right3 },
+
+    { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+    { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+    { .key = Key_D4, .length = NoteLength_Half, .finger = Finger_Right2 },
+
+    { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_G4, .length = NoteLength_Quarter, .finger = Finger_Right5 },
+    { .key = Key_G4, .length = NoteLength_Half, .finger = Finger_Right5 },
+
+    { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+    { .key = Key_C4, .length = NoteLength_Quarter, .finger = Finger_Right1 },
+    { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+
+    { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_C4, .length = NoteLength_Quarter, .finger = Finger_Right1 },
+
+    { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+    { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+    { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+
+    { .key = Key_C4, .length = NoteLength_Whole, .finger = Finger_Right1 },
+
+    { .key = Key_Invalid, .length = 0, .finger = 0 }
+};
+
+const Note_t cMajorScale1[] = {
+    { .key = Key_C3, .length = NoteLength_Quarter, .finger = Finger_Left5 },
+    { .key = Key_D3, .length = NoteLength_Quarter, .finger = Finger_Left4 },
+    { .key = Key_E3, .length = NoteLength_Quarter, .finger = Finger_Left3 },
+    { .key = Key_F3, .length = NoteLength_Quarter, .finger = Finger_Left2 },
+    { .key = Key_G3, .length = NoteLength_Quarter, .finger = Finger_Left1 },
+    { .key = Key_A3, .length = NoteLength_Quarter, .finger = Finger_Left3 },
+    { .key = Key_B3, .length = NoteLength_Quarter, .finger = Finger_Left2 },
+    { .key = Key_C4, .length = NoteLength_Quarter, .finger = Finger_Left1 },
+
+    { .key = Key_B3, .length = NoteLength_Quarter, .finger = Finger_Left2 },
+    { .key = Key_A3, .length = NoteLength_Quarter, .finger = Finger_Left3 },
+    { .key = Key_G3, .length = NoteLength_Quarter, .finger = Finger_Left1 },
+    { .key = Key_F3, .length = NoteLength_Quarter, .finger = Finger_Left2 },
+    { .key = Key_E3, .length = NoteLength_Quarter, .finger = Finger_Left3 },
+    { .key = Key_D3, .length = NoteLength_Quarter, .finger = Finger_Left4 },
+    { .key = Key_C3, .length = NoteLength_Quarter, .finger = Finger_Left5 },
+
+    { .key = Key_Invalid, .length = 0, .finger = 0 }
+};
+
+const Note_t cMajorScale2[] = {
+    { .key = Key_C4, .length = NoteLength_Quarter, .finger = Finger_Right1 },
+    { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+    { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_F4, .length = NoteLength_Quarter, .finger = Finger_Right1 },
+    { .key = Key_G4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+    { .key = Key_A4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_B4, .length = NoteLength_Quarter, .finger = Finger_Right4 },
+    { .key = Key_C5, .length = NoteLength_Quarter, .finger = Finger_Right5 },
+
+    { .key = Key_B4, .length = NoteLength_Quarter, .finger = Finger_Right4 },
+    { .key = Key_A4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_G4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+    { .key = Key_F4, .length = NoteLength_Quarter, .finger = Finger_Right1 },
+    { .key = Key_E4, .length = NoteLength_Quarter, .finger = Finger_Right3 },
+    { .key = Key_D4, .length = NoteLength_Quarter, .finger = Finger_Right2 },
+    { .key = Key_C4, .length = NoteLength_Quarter, .finger = Finger_Right1 },
+
+    { .key = Key_Invalid, .length = 0, .finger = 0 }
+};

18b6c23f0dce573b0fa18f24bb91dc06f4c86601
parchule <parchule@purdue.edu>
2022-02-16 16:26:10 -0500

    Working BLE

--- bluepySender.py
+++ bluepySender.py
@@ -1,18 +1,40 @@
 # Prerequisites: sudo apt-get install libglib2.0-dev; sudo pip install bluepy
 # References: https://elinux.org/RPi_Bluetooth_LE, https://ianharvey.github.io/bluepy-doc/
 from bluepy import btle
+# Prerequisites: sudo pip install RPi.GPIO
+# References: https://pypi.org/project/RPi.GPIO/ 
+import RPi.GPIO as GPIO
 
-print "Connecting..."
-dev = btle.Peripheral("62:00:A1:21:6E:67")
+# Initializing Pi GPIO Port 18 (Blue LED)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18, GPIO.OUT)
+GPIO.output(18, 0) # Pin 18 (Blue LED) off
 
-passthroughUuid = btle.UUID("0000ffe0-0000-1000-8000-00805f9b34fb")
-passthroughService = dev.getServiceByUUID(passthroughUuid)
+# Connection attempt loop
+loopVar = True
+atm = 1 # current connection attempt
+while loopVar:
+    print("Attempt at Connection " + str(atm))
+    try:
+        # btle.Peripheral throws btle.BTLEDisconnectError if pairing unsuccessful
+        dev = btle.Peripheral("62:00:A1:21:6E:67")
+        passthroughUuid = btle.UUID("0000ffe0-0000-1000-8000-00805f9b34fb")
+        passthroughService = dev.getServiceByUUID(passthroughUuid)
 
-writeUuid = btle.UUID("0000ffe9-0000-1000-8000-00805f9b34fb")
-writeCharacteristic = passthroughService.getCharacteristics(writeUuid)[0]
+        writeUuid = btle.UUID("0000ffe9-0000-1000-8000-00805f9b34fb")
+        writeCharacteristic = passthroughService.getCharacteristics(writeUuid)[0]
 
-print "Ready to send"
-
+        GPIO.output(18, 1) # Pin 18 (Blue LED) on
+        print("Pairing Successful")
+        loopVar = False
+    except btle.BTLEDisconnectError:
+        print("Connection Attempt " + str(atm) + " Failed")
+    atm += 1
+    
+# Values to send 0 - 87 + 88 (e)
 while True:
-    userInput = raw_input()
-    writeCharacteristic.write(bytes(str(userInput)))
+    # Sample array data
+    num = [88, 88, 88, 88, 88, 87, 43, 34, 50, 55]
+    byteNum = bytes(num)
+    print(byteNum)
+    writeCharacteristic.write(byteNum)
